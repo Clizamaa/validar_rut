88851821a688276bf3c760dfd567687f13f392f0
Clizamaa <86389249+Clizamaa@users.noreply.github.com>
2021-07-08 00:31:50 -0400

    Valida Rut

---
+++ validarRUT.js
@@ -0,0 +1,35 @@
+﻿function checkRut(rut) {
+    
+    var valor = rut.value.replace('.', '');
+    
+    valor = valor.replace('-', '');
+    
+    cuerpo = valor.slice(0, -1);
+    dv = valor.slice(-1).toUpperCase();
+    
+    rut.value = cuerpo + '-' + dv
+    
+    if(cuerpo.length < 7 || cuerpo.length > 8) { rut.setCustomValidity("RUT Erroneo"); return false; }
+
+    suma = 0;
+    multiplo = 2;
+
+    for (i = 1; i <= cuerpo.length; i++) {
+
+        index = multiplo * valor.charAt(cuerpo.length - i);
+
+        suma = suma + index;
+
+        if (multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+
+    }
+
+    dvEsperado = 11 - (suma % 11);
+
+    dv = (dv == 'K') ? 10 : dv;
+    dv = (dv == 0) ? 11 : dv;
+
+    if (dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+
+    rut.setCustomValidity('');
+}
